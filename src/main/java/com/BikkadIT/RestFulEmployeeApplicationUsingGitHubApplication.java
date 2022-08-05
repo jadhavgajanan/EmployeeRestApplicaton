412dd126cd5adf4640892fadb1f7440f10440e3a
jadhavgajanan <gajananjadhav1020@gmail.com>
2022-08-05 14:44:46 +0530

    start the implementation

---
+++ src/main/java/com/BikkadIT/RestFulEmployeeApplicationUsingGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulEmployeeApplicationUsingGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulEmployeeApplicationUsingGitHubApplication.class, args);
+	}
+
+}
